e2d17b6b8701b883c435aa597498d6d1ea1b729c
Amey <96690322+Coding-Storm@users.noreply.github.com>
2021-12-27 20:25:05 +0530

    Add files via upload

---
+++ pr.bat
@@ -0,0 +1,183 @@
+@echo off
+color 2
+Title Setting Up..
+echo.
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: I 1%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: II 2%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: III 3%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIII 10%%
+echo --------------------------------------------------------------------------------
+ping -n 2 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIII 15%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIII 20%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIII 25%%
+echo --------------------------------------------------------------------------------
+ping -n 2 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIII20%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIII25%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIII40%%
+echo --------------------------------------------------------------------------------
+ping -n 2 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIII 45%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIII 50%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIII 55%%
+echo --------------------------------------------------------------------------------
+ping -n 2 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIII60%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIII 65%%
+echo --------------------------------------------------------------------------------
+ping -n 2 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 70%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 80%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 85%%
+echo --------------------------------------------------------------------------------
+ping -n 2 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 90%%
+echo --------------------------------------------------------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 95%%
+echo --------------------------------------------------------------------------------
+ping -n 2 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo --------------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 100%%
+echo --------------------------------------------------------------------------------
+GOTO END
+:END
+cls
+echo.
+echo.
+Title Setting Up Complete
+echo Setup complete...
+echo ------------------------------------------------------------------------
+echo Progress: IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII 100%%
+echo.
+echo.
+pause
